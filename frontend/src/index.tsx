b19dd7afba4b69771762b0bbb7e45f2219a6975c
Shin <n11145862@qut.edu.au>
2025-03-06 09:30:42 +1000

    Fix errors to starts the app: 1. index.tsx:update the name for react-query. 2.homepage.tsx-add a return statement

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider } from 'react-query';
+// import { QueryClient, QueryClientProvider } from 'react-query';
+// import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// @tanstack/react-query
 
 const queryClient = new QueryClient();
 
