5800f6018e193e79e15f01c59e57763c32649dd9
peachnono <nhordejan@gmail.com>
2025-03-13 11:06:16 +1000

    Getting the frontend to connect to the backend

--- backend/src/users/auth.controller.ts
+++ backend/src/users/auth.controller.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 // auth.controller.ts
 import { Controller, Post, Get, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
