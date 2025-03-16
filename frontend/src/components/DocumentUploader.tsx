e885470c85724bd002178be70f26b0d7a0ca6776
naomi <naomi.pham@connect.qut.edu.au>
2025-03-16 16:47:08 +1000

    Added pdf previewer

--- frontend/src/components/DocumentUploader.tsx
+++ frontend/src/components/DocumentUploader.tsx
@@ -2,6 +2,18 @@ import React, { useState } from 'react';
 import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 
+// Image preview plugin
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
+// PDF preview plugin
+// @ts-ignore
+import FilePondPluginPdfPreview from 'filepond-plugin-pdf-preview';
+import 'filepond-plugin-pdf-preview/dist/filepond-plugin-pdf-preview.css';
+
+// Registering the Plugins
+registerPlugin(FilePondPluginImagePreview, FilePondPluginPdfPreview);
+
 interface DocumentUploaderProps {
   onFilesUpdate?: (files: File[]) => void; // Optional callback to pass files up
 }
@@ -21,6 +33,7 @@ const DocumentUploader: React.FC<DocumentUploaderProps> = ({ onFilesUpdate }) =>
       }}
       name="filepond" // FilePond requires the field name to match
       allowMultiple={true}
+      allowImagePreview={true} // Ensures previews are enabled
       maxFiles={3}
       server={{
         url: "http://localhost:3000", // Base API URL
